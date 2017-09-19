dcb7da2f2806d6f77faac2fb71190742c7100672
huays <huayongsheng01@chinatopcredit.com>
2017-09-19 18:04:24 +0800

    test

--- learn/src/Generics/Demo01.java
+++ learn/src/Generics/Demo01.java
@@ -1,4 +1,70 @@
 package Generics;
 
+/**
+ * 泛型类 & 泛型方法
+ */
 public class Demo01 {
+
+    public static void main(String[] args) {
+//        Box<Integer> integerBox = new Box<>();
+//        integerBox.setT(1);
+//        System.out.println(integerBox.getT());
+        Pair<Integer, String> p1 = new Pair<>(1, "java");
+        Pair<Integer, String> p2 = new Pair<>(2, "python");
+        System.out.println(Util.compare(p1, p2));
+    }
+
 }
+
+
+class Box<T> {
+
+    private T t;
+
+    public T getT() {
+        return t;
+    }
+
+    public void setT(T t) {
+        this.t = t;
+    }
+}
+
+class Pair<K, V> {
+
+    private K key;
+
+    private V value;
+
+    public Pair(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+
+    public K getKey() {
+        return key;
+    }
+
+    public void setKey(K key) {
+        this.key = key;
+    }
+
+    public V getValue() {
+        return value;
+    }
+
+    public void setValue(V value) {
+        this.value = value;
+    }
+}
+
+
+class Util {
+
+    public static <K, V> boolean compare(Pair<K, V> p1, Pair<K, V> p2) {
+        return p1.getKey().equals(p2.getKey()) &&
+                p1.getValue().equals(p2.getValue());
+    }
+}
+
+
--- learn/src/concurrent/TestCountDownLatch.java
+++ learn/src/concurrent/TestCountDownLatch.java
@@ -1,4 +1,9 @@
 package concurrent;
 
 public class TestCountDownLatch {
+
+
+
+
+
 }
